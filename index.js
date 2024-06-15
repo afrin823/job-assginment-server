aaf0ac3b30b694bca3efa716c55cc42e82c95529
afrin823 <afrin12490@gmail.com>
2024-06-15 10:44:57 +0600

    create assignment

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-require('dotenv').config()
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const  cors = require('cors');
 const port = process.env.PORT || 4000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nshaxle.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Use environment variables for MongoDB credentials
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vadwj9m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+ 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -16,19 +20,18 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  } 
+  }
 });
- const AddedNewsCollection = client.db('nahar12490').collection('assignment');
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
-// ______________users related api___________________//
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error(error);
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -36,9 +39,14 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) =>{
-    res.send('news coming soon')
-})
-app.listen(port, () =>{
-    console.log(`assignment coming on the port ${port}`);
-})
+// Route
+app.get('/', (req, res) => {
+  res.send('Assignment is running');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Assignment server is running on port: ${port}`);
+});
+
+
