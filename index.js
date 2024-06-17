335a0a7b079562e9ad1319ef16060f91b318a330
afrin823 <afrin12490@gmail.com>
2024-06-17 19:08:54 +0600

    create givemark

--- index.js
+++ index.js
@@ -1,52 +1,191 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
-// Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:4000",
+      "https://earthwish-c17dd.web.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
-
-// Use environment variables for MongoDB credentials
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vadwj9m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
- 
+const uri = `mongodb+srv://${process.env.DB_PENDING}:${process.env.DB_PENDINGPASS}@cluster0.vadwj9m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+const cookieOption = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+};
+
+const logger = async (req, res, next) => {
+  next();
+};
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "not authorized" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 async function run() {
   try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } catch (error) {
-    console.error(error);
+    const assignmentCollection = client
+      .db("afrinAssignment")
+      .collection("assignmentCollection");
+
+    const bidsCollection = client.db("assignmentDB").collection("bids");
+    console.log(bidsCollection);
+
+    app.post("/assignment", async (req, res) => {
+      const newAssignment = req.body;
+      const result = await assignmentCollection.insertOne(newAssignment);
+      res.send(result);
+    });
+
+    app.get("/assigment", async (req, res) => {
+        try {
+          const { level } = req.query;
+
+          // console.log("form valid token", req.user);
+          // if (req.query.email !== req?.user.email) {
+          //   return res.status(403).send({ message: "forbidden access" });
+          // }
+          
+          let query = {};
+
+          if (level && level.length > 0) {
+            query = { level: level }; // Assuming 'level' is a field in your MongoDB documents
+          }
+
+          const cursor = await assignmentCollection.find(query);
+          const result = await cursor.toArray();
+
+          res.send(result);
+        } catch (error) {
+          console.error("Error fetching assignments:", error);
+          res.status(500).send("Internal Server Error");
+        }
+      }
+    );
+
+    app.put("/assignment/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updated = req.body;
+      const assignment = {
+        $set: {
+          titleName: updated.titleName,
+          description: updated.description,
+          processingTime: updated.processingTime,
+          mark: updated.mark,
+          photo: updated.photo,
+          level: updated.level,
+        },
+      };
+      const result = await assignmentCollection.updateOne(
+        filter,
+        assignment,
+        options
+      );
+      res.send(result);
+    });
+
+    app.post("/bids", async (req, res) => {
+      const bids = req.body;
+      const result = await bidsCollection.insertOne(bids);
+      res.send(result);
+    });
+
+    app.get("/bids/pending", async (req, res) => {
+      const query = { status: { $eq: "pending" } };
+      const result = await bidsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bidsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.put("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updated = req.body;
+      const update = {
+        $set: {
+          givenMark: updated.givenMark,
+          feedBack: updated.feedBack,
+          status: updated.status,
+        },
+      };
+      const result = await bidsCollection.updateOne(query, update, options);
+      res.send(result);
+    });
+
+    app.get("/bid/:email", async (req, res) => {
+      const user = req.params.email;
+      const filter = { examineeEmail: user };
+      const result = await bidsCollection.find(filter).toArray();
+      res.send(result);
+    });
+
+    app.delete("/assigment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await assignmentCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/assigmentCount", async (req, res) => {
+      const count = await assignmentCollection.estimatedDocumentCount();
+      res.send({ count });
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
 
-// Route
-app.get('/', (req, res) => {
-  res.send('Assignment is running');
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
 
-// Start server
 app.listen(port, () => {
-  console.log(`Assignment server is running on port: ${port}`);
+  console.log(`Running on port: ${port}`);
 });
-
-
